fc1d687ccdfbd3cd6dbed6ac0eea26574fa040a8
gnanasekart <gnanasekar69@hotmail.com>
2020-08-09 09:00:21 +0530

    commit

---
+++ src/Features/Login.feature
@@ -0,0 +1,11 @@
+Feature: Login into CRM site
+  This feature to verify the login feature
+
+  Scenario: Login with valid credential details
+    Given navigate to login page
+    When enter login credential details
+    And click login button
+    Then page should login successfully
+    And verify crmsfa button
+    And verify logout button
+
---
+++ src/test/java/Cucumber/org/Login.java
@@ -0,0 +1,40 @@
+package Cucumber.org;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java8.En;
+
+public class Login implements En {
+
+    @Given("^navigate to login page$")
+    public void navigatetologinpage() {
+
+    }
+
+    @When("enter login credential details")
+    public void enterLoginCredentialDetails() {
+
+    }
+
+    @And("click login button")
+    public void clickLoginButton() {
+
+    }
+
+    @Then("page should login successfully")
+    public void pageShouldLoginSuccessfully() {
+
+    }
+
+    @And("verify crmsfa button")
+    public void verifyCrmsfaButton() {
+
+    }
+
+    @And("verify logout button")
+    public void verifyLogoutButton() {
+
+    }
+}
